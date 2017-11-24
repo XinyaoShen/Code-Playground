4e0524c6a94c69cc1797023692fefb76e803654c
ultmaster <scottyugochang@hotmail.com>
2017-11-24 23:47:44 +0800

    fix: allow hosts

--- CodePlayground/settings.py
+++ CodePlayground/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wm49q0$s*k5z@fzhduu3#*zu&j1!a!-i$@m1hd$=r2)#6v1*b9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
